b73b79a1ae69bf8ced3fd6fbb3383dfdd1ff47ed
Vedant Madhwe <105602657+VedantMadhwe@users.noreply.github.com>
2024-09-13 01:24:52 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0oi-8hfhv28xty35^z)gk21nr0(jm2-%9ub^2bh=-xaeazrryi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
